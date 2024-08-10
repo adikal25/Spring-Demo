2977b030ce042fd699b32d6282009d780c6daef3
adikal25 <ka584@@snu.edu.in>
2024-08-10 23:29:32 +0530

    Added Actuators endpoints

---
+++ demoapp/src/test/java/com/love2kode/springboot/demo/demoapp/DemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love2kode.springboot.demo.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
